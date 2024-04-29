0a0f5af820fb40d9e6d4fb842c2f5e6b69847cec
WellingtonGabriel1 <WellingtonGabriel7700@gmail.com>
2024-04-29 08:32:13 -0300

    feat: finish packages section and opinion peoples section

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const buttn = document.querySelectorAll(".butn");
 
+// Função para redefinir a cor de todos os botões
+function resetButtons() {
+  buttn.forEach((button) => {
+    button.style.backgroundColor = "#808080";
+    button.dataset.toggled = "false";
+  });
+}
+
 buttn.forEach((button) => {
   button.dataset.toggled = "false";
   button.addEventListener("click", () => {
     if (button.dataset.toggled === "false") {
+      resetButtons(); // Redefinir todos os botões quando um é clicado
       button.style.backgroundColor = "#00a651";
       button.dataset.toggled = "true";
     } else {
