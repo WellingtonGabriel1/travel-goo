e32887f801b367e098d06d2c96e158671680ba60
WellingtonGabriel1 <WellingtonGabriel7700@gmail.com>
2024-06-04 07:44:55 -0300

    feat: implement question page and travel modal card

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 const buttn = document.querySelectorAll(".butn");
+const arrow1 = document.querySelector(".arrow1");
+const arrow2 = document.querySelector(".arrow2");
+const arrow3 = document.querySelector(".arrow3");
+const container1 = document.querySelector(".destinations-modal-container1");
+const container2 = document.querySelector(".destinations-modal-container2");
+const container3 = document.querySelector(".destinations-modal-container3");
+
+arrow1.addEventListener("click", function () {
+  if (container1.style.display === "block") {
+    container1.style.display = "none";
+  } else {
+    container1.style.display = "block";
+  }
+});
+
+arrow2.addEventListener("click", function() {
+  if (container2.style.display === "block") {
+    container2.style.display = "none";
+  } else {
+    container2.style.display = "block";
+  }
+});
+
+arrow3.addEventListener("click", function() {
+  if (container3.style.display === "block") {
+    container3.style.display = "none";
+  } else {
+    container3.style.display = "block";
+  }
+});
 
 // Função para redefinir a cor de todos os botões
 function resetButtons() {
@@ -22,4 +52,6 @@ buttn.forEach((button) => {
   });
 });
 
-
+function onClickScrollTo(id) {
+  document.getElementById(id).scrollIntoView();
+}
